c3560040a2c78d09692668c3cde21e3716be02b4
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2017-09-05 18:38:44 +0900

    bunlde gem

---
+++ lib/rubocop/verbose.rb
@@ -0,0 +1,7 @@
+require "rubocop/verbose/version"
+
+module Rubocop
+  module Verbose
+    # Your code goes here...
+  end
+end
---
+++ lib/rubocop/verbose/version.rb
@@ -0,0 +1,5 @@
+module Rubocop
+  module Verbose
+    VERSION = "0.1.0"
+  end
+end
